cff1c8a89c404b357eb62fe64274177f0c67a4e6
Alpay.K <allpay_66_@hotmail.de>
2024-07-29 11:26:42 +0200

    changing greet function

---
+++ summary.js
@@ -0,0 +1,18 @@
+(function() {
+    const greetingElement = document.getElementById('greeting');
+    const now = new Date();
+    const hours = now.getHours();
+
+    let greetingText;
+    if (hours < 12) {
+        greetingText = 'Good Morning';
+    } else if (hours < 18) {
+        greetingText = 'Good Afternoon';
+    } else if (hours < 22) {
+        greetingText = 'Good Evening';
+    } else {
+        greetingText = 'Good Night';
+    }
+
+    greetingElement.textContent = greetingText;
+})();
